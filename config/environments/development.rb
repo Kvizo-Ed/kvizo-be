0957d43cdf544c9b193b03da5af920a6e589aa48
Nathan Brown <nathan.brown263@gmail.com>
2022-02-10 15:23:21 -0700

    feat: Finish websockets tutorial

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, nil]
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
