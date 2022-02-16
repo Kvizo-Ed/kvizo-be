396bdad53adb1c9163ad23aab22caf012c2ebc1e
Nathan Brown <nathan.brown263@gmail.com>
2022-02-16 11:51:42 -0700

    feat: Working websockets!!!!!!!

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,10 +2,11 @@ class Api::V1::QuestionsController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def create
-    question = Question.new(quiz_id: "1", question_text: params[:question_text], question_type: "TF", correct_answer: "T")
-    if question.save!
-      ActionCable.server.broadcast "questions_channel", question: question.question_text
-    end
+    question = Question.find(params[:questionID])
+
+    ActionCable.server.broadcast "questions_channel", question: QuestionSerializer.new(question)
+
+    render json: {question: 'question sent!'}, status: 201
   end
 
 end
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -5,15 +5,9 @@ class QuestionSerializer
              :question_type,
              :correct_answer
 
-  
   attribute :possible_answers do |question|
-      {
-      questionText: question.question_text,
-      questionType: question.question_type,
-      correctAnswer: question.correct_answer,
-      possibleAnswers: question.possible_answers.map do |answer|
+      question.possible_answers.map do |answer|
         answer.text
       end
-      }
   end
 end
